b54c792b1b28abaa1972881c202351906067f333
juyeon-02 <xsoomini@naver.com>
2024-11-05 06:49:16 +0900

    Redux 실습 위한 카운터 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
-import { useState } from 'react'
-import './App.scss'
+// App.jsx
+import { legacy_createStore } from 'redux';
+import './App.scss';
+import { useDispatch, useSelector } from 'react-redux';
+
+// Action 객체 정의
+const increase = { type: 'increment' };
+const decrease = { type: 'decrement' };
+
+// 리듀서 정의
+const counterReducer = (state = { counter: 0 }, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { counter: state.counter + 1 };
+    case 'decrement':
+      return { counter: state.counter - 1 };
+    default:
+      return state;
+  }
+};
+
+// Redux store 생성
+export const store = legacy_createStore(counterReducer);
 
 function App() {
+  // state에서 counter만 선택
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch()
 
   return (
-    <>
-      <div>
-        
-      </div>
-    </>
-  )
+    <div>
+      <p>카운터: {counter}</p>
+      <button onClick={()=> dispatch(increase)}>+</button>
+      <button onClick={()=> dispatch(decrease)}>-</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.scss'
-import App from './App.jsx'
+// main.jsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.scss';
+import App from './App.jsx';
+import { Provider } from 'react-redux';
+import { store } from './App.jsx'; // store 가져오기
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
